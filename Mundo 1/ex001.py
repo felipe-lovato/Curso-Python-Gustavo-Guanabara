6456c43282cf93b476b133f4efd4d46242e89c90
Felipe Lovato <felipe.lovato95@gmail.com>
2021-12-11 09:56:07 -0300

    Criacao 0.1

---
+++ Mundo 1/ex001.py
@@ -0,0 +1,10 @@
+# Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado
+
+nome = input("Digite seu nome: \n")
+print(f"Olá, {nome}! Prazer em conhecer você!")
+
+# Crie um programa que escreva "Olá, mundo" na tela.
+
+print("Olá, mundo!")
+msg = "Hello, world!"
+print(msg)
