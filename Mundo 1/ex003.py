d300ed3d7be20c1566d63af91a34c9434ee64aeb
Felipe Lovato <felipe.lovato95@gmail.com>
2021-12-11 10:07:20 -0300

    adicionando exercicio 2 e 3

---
+++ Mundo 1/ex003.py
@@ -0,0 +1,6 @@
+#Exercício Python 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = int(input('Digite um valor: '))
+num2 = int(input('Digite outro valor: '))
+s = num1 + num2
+print(f'A sina entre {num1} e {num2} é igual a {s}')
