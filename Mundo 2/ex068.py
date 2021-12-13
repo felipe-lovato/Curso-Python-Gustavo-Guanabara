fb3c280cb020225b76647215dd38f7bd4d61cdd4
Felipe Lovato <felipe.lovato95@gmail.com>
2021-12-12 22:25:43 -0300

    add

--- Mundo 2/ex068.py
+++ Mundo 2/ex068.py
@@ -1,12 +1,18 @@
 # Exercício Python 068: Faça um programa que jogue par ou ímpar com o computador. O jogo só será interrompido quando o
 # jogador perder, mostrando o total de vitórias consecutivas que ele conquistou no final do jogo.
+
 from random import randint
+ponto = 0
 while True:
-    print('Jogo Impar ou Par')
-    escolha = int(input('[1] Impar\n[2] Par\n'))
-    op = randint(1, 2)
-    print(op)
-    if escolha == op:
-        print(f'Deu {"IMPAR" if op == 1 else "PAR"} Aee acertou!')
+    numero = int(input('Digite um valor :'))
+    opcao = input('Par ou Impar [P/I]').upper()
+    op = randint(1, 100)
+    resul = 'P' if op % 2 == 0 else 'I'
+    if resul == op:
+        print(f'Você Venceu, Numero:{op+numero} - {"PAR" if (op+numero) % 2 == 0 else "IMPAR"}')
+        ponto += 1
     else:
-        print(f'Deu {"IMPAR" if op == 1 else "PAR"} Não foi desse vez')
+        print(f'Você Perdeu, Numero:{op + numero} - {"PAR" if (op + numero) % 2 == 0 else "IMPAR"}')
+        break
+print(f'Total de vitoria = {ponto}')
+print('Finalizando..')
